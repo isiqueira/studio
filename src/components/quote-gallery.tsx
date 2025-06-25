66a6c30c1b26225f80b90e7acd5fa2eef6798a4a
Italo Siqueira <siqueira.italo@gmail.com>
2025-06-25 19:34:29 +0000

    change the width of card to 30% of div area

--- src/components/quote-gallery.tsx
+++ src/components/quote-gallery.tsx
@@ -18,7 +18,7 @@ export default function QuoteGallery({ quotes }: QuoteGalleryProps) {
   }
   
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
       {quotes.map((quote) => (
         <QuoteCard key={quote.id} quote={quote} />
       ))}
