11101dbb66d2251f71ab8380c543be23d9e66981
Italo Siqueira <siqueira.italo@gmail.com>
2025-06-26 19:24:54 +0000

    chore: html metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,15 @@ import { Toaster } from '@/components/ui/toaster';
 export const metadata: Metadata = {
   title: 'STB - Quotation',
   description: 'A STB multi-quotation app.',
+  icons: {
+    icon: '/favicon.ico',
+  },
+  viewport: {
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1,
+  },
+  themeColor: '#ffffff',
 };
 
 export default function RootLayout({
@@ -15,6 +24,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="STB Australia - Quotation" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <title>STB Australia - Quotation</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap" rel="stylesheet" />
