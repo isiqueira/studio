aa05b346e0ae40705a1344cb640fb9229bb63d0b
Italo Siqueira <siqueira.italo@gmail.com>
2025-07-02 18:12:40 +0000

    thanks, now obtain the flag from env conf

--- src/lib/feature-flags.ts
+++ src/lib/feature-flags.ts
@@ -1,2 +1,3 @@
 // Set this flag to true to fetch data from the API, or false to use local mock data.
-export const FF_FROM_API = false;
+// This can be controlled via the FF_FROM_API environment variable in your .env.local file.
+export const FF_FROM_API = process.env.FF_FROM_API === 'true';
