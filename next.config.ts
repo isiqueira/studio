f72466ed2aa1980fa92bc6b8a13e7af2b25131cd
Italo Siqueira <siqueira.italo@gmail.com>
2025-07-02 20:34:09 +0000

    please add jaragua.lmq.cloudamqp.com to next.config.ts?

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -31,6 +32,12 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'jaragua.lmq.cloudamqp.com',
         port: '',
         pathname: '/**',
       }
